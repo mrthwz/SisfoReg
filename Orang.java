0cc168846b3b4527da33a030474eec0cb3d2e151
sweet19bty <emmafadma21@gmail.com>
2016-03-12 22:30:38 +0700

    Fadma
    Kelas

--- Orang.java
+++ Orang.java
@@ -1,1 +1,44 @@
+/* 	Nama 	: Fadma Sari Yuenam E.G.
+	Kelas 	: IF - 38 - 02
+	NIM		:1301144352
+*/
 
+public abstract class Orang {
+    protected String nama;
+    protected String email;
+    protected String noInduk;
+    protected String alamat;
+	
+	
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public void setNoInduk (String noInduk){
+		this.noInduk = noInduk;
+	}
+	
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	
+	public String getNama(){
+		return nama;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public String getNoInduk(){
+		return noInduk;
+	}
+	
+	public String getAlamat(){
+		return alamat;
+	}
+}
