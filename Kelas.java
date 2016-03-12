3d5ccab383bf2cbd0c9a58e78c2ac811c94f9f73
mrthwz <daniamartha@gmail.com>
2016-03-13 00:18:03 +0800

    Create Kelas.java

---
+++ Kelas.java
@@ -0,0 +1,52 @@
+
+public class Kelas{
+    private Matakuliah mk;
+    private Dosen d;
+	public String namaKelas;
+	public String kodeKelas;
+	
+	public Kelas(String kodeKelas, String namaKelas){
+		this.namaKelas=namaKelas;
+		this.kodeKelas=kodeKelas;
+	}
+	
+	
+	public void setKodeKelas(String kodeKelas){
+		this.kodeKelas=kodeKelas;
+	}
+	
+	public void setNamaKelas(String namaKelas){
+		this.namaKelas=namaKelas;
+	}
+	
+	public String getNamaKelas(){
+		return ("Nama Kelas		: "+this.namaKelas);
+	}
+	
+	    public String getKodeKelas() {
+		return ("Kode Kelas		: "+this.kodeKelas);
+    }
+	
+	
+    public Matakuliah getMk() {
+        return mk;
+    }
+
+    public void setMk(Matakuliah mk) {
+        this.mk = mk;
+    }
+
+    public Dosen getD() {
+        return d;
+    }
+
+    public void setD(Dosen d) {
+        this.d = d;
+    }
+    
+    
+    
+    
+    
+    
+}
